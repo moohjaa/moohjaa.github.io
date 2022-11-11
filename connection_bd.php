b495d39a641ac5995b4cbcbae7ea5fe7c3f43294
Moohja <87211909+moohjaa@users.noreply.github.com>
2022-11-11 02:55:36 +0100

    Delete connection_bd.php

--- connection_bd.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conexion=new mysqli("HOST=us-east.connect.psdb.cloud","mkmgqo0k0a3mxgbuy7or","pscale_pw_dgnmPqA0NFkMpwBwBn6UNtrpqI3N3JTQkgjOBobplRS","users");
-$conexion->set_charset("UTF8");
-
-
-
-?>
